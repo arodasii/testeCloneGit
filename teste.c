b9c8838d372bc8b1f9408bcd8c1c6f677bd0344c
Caio Lemos <caio.969@outlook.com>
2020-02-14 08:24:47 -0300

    fiz o que o prof pediu

--- teste.c
+++ teste.c
@@ -1,8 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(){
+int main()
+{
+	int idade, idadeNovo;
+	
+	printf("Insira sua idade:\n");
+	scanf("%d", &idade);
 
+	if(idade > 0)
+	{	
+		idade-1 == idadeNovo;
+	}
+	else
+	{
+		printf("Idade inexistente\n");
+	}
+
+  	printf("Sua idade é: %d\n", idadeNovo);
 
     return 0;
 }
